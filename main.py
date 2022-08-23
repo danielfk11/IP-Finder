312a1469234486831a04ce41a8ad32416b665e52
Daniel Facini Kiffer <danielfkiffer@outlook.com>
2022-08-23 18:33:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import requests
 import json
 from urllib.request import urlopen
+import folium 
 
 ip = input('-> Coloque o ip que deseja acessar\n-> ')
+file = 'infos.json'
+file_save = "buscas.txt"
 
 if ip == '':
     print("-> ERROR\n-> Informe um IP")
     exit()
 
 print("Iniciando busca..{}".format(ip))
-file = 'infos.json'
 
 try:
     if ip:
@@ -18,7 +20,7 @@ try:
     r = requests.get(url_get_ip)
     rtext = r.text
 
-    with open("infos.json", "w") as textsave:
+    with open(file, "w") as textsave:
         textsave.write(rtext)
 
     request = urlopen(url_get_ip)
@@ -28,7 +30,7 @@ try:
     for i in data:
        print(f"-> {i} = {data[i]}")
 
-    with open("infos.json") as my_json:
+    with open(file) as my_json:
         dados = json.load(my_json)
         find_dados = dados
 
@@ -39,10 +41,26 @@ try:
     print("-> GOOGLE MAPS\n-> https://maps.google.com/?q={},{}".format(lat, lon))
     print("---------------------------------------------------")
 
-    with open("buscas.txt", 'a') as buscas_save:
+    with open(file_save, 'a') as buscas_save:
         buscas_save.write("\n{}".format(rtext))
 
 except:
     print("************************")
     print("-> ERROR")
     print("************************")
+
+try:
+    mapa = folium.Map([lat, lon],               #VERIFICAR ERRO
+    files='Staten Terrain',
+    zoom_start=16
+    )
+
+    addmap = folium.Marker([lat, lon],
+    popup='<i>Ip Finder Location<i>',
+    tooltip='Vizualizar').add_to(mapa)
+
+    mapa.save(r'.\mapa.html')
+    print('Mapa criado com sucesso!\nMapa -> http://127.0.0.1:5500/mapa.html')
+    print("---------------------------------------------------")
+except:
+    print("error")
